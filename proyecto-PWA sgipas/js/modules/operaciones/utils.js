d9d28a6212e66d8d7167bb9ea1d67aba15d7b6b1
SGIPAS <willalwaysloveyouy@gmail.com>
2026-07-29 12:25:55 -0400

    Delete proyecto-PWA sgipas directory

--- proyecto-PWA sgipas/js/modules/operaciones/utils.js
+++
@@ -1,7 +0,0 @@
-// ocp Utilidades compartidas
-import { supabase } from '../../supabase-client.js';
-
-export async function obtenerRolUsuario() {
-    const { data: { user } } = await supabase.auth.getUser();
-    return user?.user_metadata?.rol || 'operador';
-}
--- proyecto-PWA sgipas/js/modules/ssl/utils.js
+++
@@ -1,7 +0,0 @@
-// ocp Utilidades del módulo SSL
-import { supabase } from '../../supabase-client.js';
-
-export async function obtenerRolUsuario() {
-    const { data: { user } } = await supabase.auth.getUser();
-    return user?.user_metadata?.rol || 'operador';
-}
